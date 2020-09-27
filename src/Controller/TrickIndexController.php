eb12626476597473ac812a61621e42416ee8950d
Ubuntu <ubuntu@info@fludy.net>
2020-09-27 19:12:54 +0000

    first works

--- src/Controller/TrickIndexController.php
+++ src/Controller/TrickIndexController.php
@@ -5,11 +5,11 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; 
 use Twig\Environment;
 
-class HomeController extends AbstractController{
-
-	public function index(): Response{
-		return $this->render("pages/Home.html.twig", [
-			'menu_active' => 'home'
+class TrickIndexController extends AbstractController{
+	
+	public function list(): Response{
+		return $this->render("trick/TrickIndex.html.twig", [
+			'menu_active' => 'trickindex'
 		]);
 	}
 	
---
+++ src/Entity/Trick.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TrickRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TrickRepository::class)
+ */
+class Trick
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $trickgroup;
+
+    /**
+     * @ORM\Column(type="object", nullable=true)
+     */
+    private $picture;
+
+    /**
+     * @ORM\Column(type="object", nullable=true)
+     */
+    private $video;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getTrickgroup(): ?string
+    {
+        return $this->trickgroup;
+    }
+
+    public function setTrickgroup(?string $trickgroup): self
+    {
+        $this->trickgroup = $trickgroup;
+
+        return $this;
+    }
+
+    public function getPicture()
+    {
+        return $this->picture;
+    }
+
+    public function setPicture($picture): self
+    {
+        $this->picture = $picture;
+
+        return $this;
+    }
+
+    public function getVideo()
+    {
+        return $this->video;
+    }
+
+    public function setVideo($video): self
+    {
+        $this->video = $video;
+
+        return $this;
+    }
+}
